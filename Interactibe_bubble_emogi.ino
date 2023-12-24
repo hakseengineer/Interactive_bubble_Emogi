c4b0671cc37c3e373b7489939eb6852094f34e06
hakseengineer <143533952+hakseengineer@users.noreply.github.com>
2023-12-24 11:54:49 +0530

    Update Interactibe_bubble_emogi.ino

--- Interactibe_bubble_emogi.ino
+++ Interactibe_bubble_emogi.ino
@@ -1,4 +1,11 @@
-#include <ezButton.h>
+/* ----------------------------------------------------------------
+  Author : Hak Se Engineer
+  Date : 24/02/2023
+  license status : open source
+------------------------------------------------------------------*/
+
+// need to add these header files using manage library option in the arduino IDE.
+#include <ezButton.h> 
 #include <Servo.h>
 
 Servo myservo[2];
@@ -11,13 +18,13 @@ ezButton button(SW_PIN);
 
 int value_X = 0; // The variable to store value of the X axis
 int value_Y = 0; // The variable to store value of the Y axis
-int prev_value_X = 0; 
-int prev_value_Y = 0;
+int prev_value_X = 0;  //required to make smooth transition
+int prev_value_Y = 0;  //required to make smooth transition
 
 void setup() {
 
-  myservo[0].attach(9);
-  myservo[1].attach(6);
+  myservo[0].attach(9);  //servo motor connected to Arduino pin 9
+  myservo[1].attach(6);  //servo motor connected to Arduino pin 6
   
  // Serial.begin(9600) ;
 }
@@ -26,14 +33,17 @@ void loop() {
   button.loop(); // MUST call the loop() function first
   
   // read analog X and Y analog values
-  value_X = map (analogRead(VRX_PIN), 0, 1023, 0, 180);
+  value_X = map (analogRead(VRX_PIN), 0, 1023, 0, 180);  //mapping joystick value with servo angle 
   value_Y = map (analogRead(VRY_PIN), 0, 1023, 0, 180);
 
-  //Serial.print("x = ");
-  //Serial.print(value_X);
-  //Serial.print(", y = ");
-  //Serial.println(value_Y);
-
+  //For debug purpose...
+  /*
+  Serial.print("x = ");
+  Serial.print(value_X);
+  Serial.print(", y = ");
+  Serial.println(value_Y);
+  */
+  
   if((value_X > prev_value_X +3)||(value_X < prev_value_X -3))
   {
         myservo[0].write(value_X); 
